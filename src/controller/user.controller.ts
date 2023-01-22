9bb093e40d9eb6d85ad0bcfd9a70a5bf43463471
aurora <aurora.makovac@gmail.com>
2023-01-22 21:59:47 +0100

    ommits password

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,7 +1,8 @@
 import { Request, Response } from "express";
+import { omit } from 'lodash';
 import { CreateUserInput } from "../schema/user.schema";
 import { createUser } from "../service/user.service";
-import loggger from '../utils/logger'
+import loggger from '../utils/logger';
 
 export async function createUserHandler(
     req: Request<{}, {}, CreateUserInput["body"]>,
@@ -9,7 +10,7 @@ export async function createUserHandler(
 ) {
     try {
         const user = await createUser(req.body);
-        return res.send(user);
+        return res.send(omit(user.toJSON(), "password"));
     } catch (e: any) {
         loggger.error(e);
         return res.status(409).send(e.message);
