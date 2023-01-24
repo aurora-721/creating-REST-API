d09235a48538118c9dd966f41628a18d9d87f7e4
aurora <aurora.makovac@gmail.com>
2023-01-24 14:42:49 +0100

    adding session

--- config/default.ts
+++ config/default.ts
@@ -4,8 +4,29 @@ export default {
     saltWorkFactor: 10,
     accessTokenTtl: "15m",
     refreshTokenTtl: "1y",
-    accessTokenPrivateKey: ``,
-    accessTokenPublicKey: ``,
+    accessTokenPrivateKey: `
+    -----BEGIN RSA PRIVATE KEY-----
+MIICXwIBAAKBgQCnn90qPutqj5WkBoZDXHsKXuGXLJmfpyuGI4Ut/D0o9cM8Lbfz
+//W200OABR7gZU12/SXtq/pxp5I9Llfd92r0dYCVWogFETsz8neajX+sFgVCaNYV
+YdnZMnhgRwBoOmAbbih8fkInOBzyixdUFIqxR7iY/5IEmuRvXHbKD0DPSwIDAQAB
+AoGBAJj+j9XkUekUHuf0UI2hFmiTy2G5Aoq3AUHQn9kmFgiRsF33+KOLZxE06LIr
+elAkBj1ptzmkM9WStiaDaw2cqw/yy3sg9hEnLjqZvv+9uBuyqAO74DEMxU1B+ZrW
+b69SX12Sfh5tbJxsbkIsQ5TS1Jej2cbMdEGUhLDvA0mdspYpAkEA52eUSrTdzbZq
+/dCaoJQVAQLncH4LjSG/NajLr1ZFUJPS5YjrgsQ6P7qh/x8vyJjiI8wdou+Zmz8p
+UkVLinl+pwJBALlw2rJOzXPoaMPJyH7pJQhnAidOOsc7dF2QJbEfVmkwgHP/EqoE
+yfHhVqJyi7FjpuaHhljO4lS89KQMXoYHAr0CQQC6ARlC/6U2gMX5pV9VahKEEJiG
+ZNrWI+U1taucEcV/ooMoO+nK3/5OUa97ic73+GP2EFFWMjAPiTKmzcqN0WELAkEA
+kv1tnODx4epbALa1gkNz41EjV9yeosXx2ygjeF6KtB6J82hAdxwMpvojHekF6bo/
+1YOfbk2vAdQ1ZsOXOU3dUQJBALjsLDV3Tpqf35yqmBrVuVw/Vw8yau3eAJeIdDQ5
+r7ZYvnaWGzCYWQOMZekMNdplscN2Hh9MV0H1FkpzUbxN84A=
+-----END RSA PRIVATE KEY-----`,
+    accessTokenPublicKey: `
+    -----BEGIN PUBLIC KEY-----
+MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCnn90qPutqj5WkBoZDXHsKXuGX
+LJmfpyuGI4Ut/D0o9cM8Lbfz//W200OABR7gZU12/SXtq/pxp5I9Llfd92r0dYCV
+WogFETsz8neajX+sFgVCaNYVYdnZMnhgRwBoOmAbbih8fkInOBzyixdUFIqxR7iY
+/5IEmuRvXHbKD0DPSwIDAQAB
+-----END PUBLIC KEY-----`,
     refreshTokenPrivateKey: ``,
     refreshTokenPublicKey: ``,
   };
--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -1,4 +1,5 @@
-import { Omit } from "lodash";
+import { FilterQuery } from "mongoose";
+import { omit } from "lodash";
 import { DocumentDefinition } from "mongoose";
 import UserModel, { UserDocument } from "../models/user.model";
 
@@ -9,4 +10,28 @@ export async function createUser(input: DocumentDefinition<
     } catch (e: any) {
         throw new Error(e);
     }
+}
+
+export async function findUser(query: FilterQuery<UserDocument>) {
+    return UserModel.findOne(query).lean();
+  }
+
+export async function validatePassword({
+    email,
+    password,
+  }: {
+    email: string;
+    password: string;
+  }) {
+    const user = await UserModel.findOne({ email });
+  
+    if (!user) {
+      return false;
+    }
+  
+    const isValid = await user.comparePassword(password);
+  
+    if (!isValid) return false;
+  
+    return omit(user.toJSON(), "password");
 }
