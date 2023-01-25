4036ca60c3a9e29851aaeca8189bd4eda13008dc
aurora <aurora.makovac@gmail.com>
2023-01-25 15:24:58 +0100

    adding README description

--- src/controller/session.controller.ts
+++ src/controller/session.controller.ts
@@ -38,3 +38,22 @@ export async function createUserSessionHandler(req: Request, res: Response) {
 
   return res.send({ accessToken, refreshToken });
 }
+
+export async function getUserSessionsHandler(req: Request, res: Response) {
+  const userId = res.locals.user._id;
+
+  const sessions = await findSessions({ user: userId, valid: true });
+
+  return res.send(sessions);
+}
+
+export async function deleteSessionHandler(req: Request, res: Response) {
+  const sessionId = res.locals.user.session;
+
+  await updateSession({ _id: sessionId }, { valid: false });
+
+  return res.send({
+    accessToken: null,
+    refreshToken: null,
+  });
+}
